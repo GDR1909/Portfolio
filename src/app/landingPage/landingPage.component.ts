f0a29383ea9b7a07949873ad0a158b53cc8a9616
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-02-16 10:55:59 +0100

    finished landing page

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingPage',
+  standalone: true,
+  imports: [],
+  templateUrl: './landingPage.component.html',
+  styleUrl: './landingPage.component.scss'
+})
+export class LandingPageComponent {
+
+}

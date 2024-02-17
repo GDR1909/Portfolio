8790947336e67e41b779a74c081ef69e68aa85fc
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-02-17 14:14:00 +0100

    started with the my skills section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LandingPageComponent } from './landingPage/landingPage.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ArrowComponent } from './arrow/arrow.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, AboutMeComponent, ArrowComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, AboutMeComponent, ArrowComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

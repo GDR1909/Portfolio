afa12c00e1d22190624c701cd6a12f2b220df529
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-02-17 13:18:48 +0100

    added the first arrow component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ArrowComponent } from './arrow/arrow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, AboutMeComponent, ArrowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

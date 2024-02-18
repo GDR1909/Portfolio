53bdb7118090b55cb1755fe7f7b9bb938541e579
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-02-18 20:12:10 +0100

    added icons to the my skills section

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+  iconFullPath = 'assets/skillIcons/';
 
+  iconSinglePath = [
+    'angular.png',
+    'typescript.png',
+    'javascript.png',
+    'html.png',
+    'css.png',
+    'firebase.png',
+    'git.png',
+    'scrum.png',
+    'api.png',
+    'materialDesign.png',
+    'scss.png',
+    'wordpress.png'
+  ];
+
+  iconName = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'Firebase',
+    'Git',
+    'Scrum',
+    'Rest-API',
+    'Material Design',
+    'SCSS',
+    'WordPress'
+  ];
 }

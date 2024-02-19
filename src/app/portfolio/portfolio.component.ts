7f31f7edfdf48db1652c71b0d8e5d5e195543174
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-02-19 19:56:30 +0100

    added some projects to my portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      'img': 'assets/portfolioImg/elPolloLoco.png',
+      'name': 'El Pollo Loco',
+      'usedTech': 'JavaScript | HTML | CSS | OOP',
+      'description': '',
+      'gitHubURL': '',
+      'liveTestURL': ''
+    },
+    {
+      'img': 'assets/portfolioImg/pokedex.png',
+      'name': 'Pokedex',
+      'usedTech': 'JavaScript | HTML | CSS | API',
+      'description': '',
+      'gitHubURL': '',
+      'liveTestURL': ''
+    },
+    {
+      'img': 'assets/portfolioImg/join.png',
+      'name': 'Join',
+      'usedTech': 'JavaScript | HTML | CSS',
+      'description': '',
+      'gitHubURL': '',
+      'liveTestURL': ''
+    }
+  ];
 }

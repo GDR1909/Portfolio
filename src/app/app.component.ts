02d842d0a8b48e148d8f3509db0c8570a1e12365
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-02-19 19:00:48 +0100

    start with the portfolio section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,25 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { ArrowComponent } from './arrow/arrow.component';
+import { ArrowRightComponent } from './arrow-right/arrow-right.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ArrowLeftComponent } from './arrow-left/arrow-left.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, HeaderComponent, AboutMeComponent, ArrowComponent, MySkillsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LandingPageComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    ArrowRightComponent,
+    MySkillsComponent,
+    ArrowLeftComponent,
+    PortfolioComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

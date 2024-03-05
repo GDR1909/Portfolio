fc8549b052c7ca5d23bb256dac6245b81be59056
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-03-05 16:45:49 +0100

    the menu will close after clicking on a link

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { PortfolioService } from '../portfolio.service';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
   constructor(private portfolioService: PortfolioService) { }
 
+
+  closeMenu() {
+    this.portfolioService.closeMenu();
+  }
 }

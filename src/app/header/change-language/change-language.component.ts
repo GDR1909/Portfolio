21c7db7d5075724063fbe5da394007312dc44cd0
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-03-22 18:37:18 +0100

    started to optimized the change language component

---
+++ src/app/header/change-language/change-language.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-language',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './change-language.component.html',
+  styleUrl: './change-language.component.scss'
+})
+export class ChangeLanguageComponent {
+  changeLanguageDropdownMenuIsOpen = false;
+  selectedLanguage: string = 'DEU';
+  languages = ['DEU', 'ENG', 'ITA'];
+  clickFunction: () => void = this.openDropdown.bind(this);
+
+
+  openDropdown() {
+    if (!this.changeLanguageDropdownMenuIsOpen) {
+      this.clickFunction = this.closeDropdown.bind(this);
+      document.getElementById('changeLanguageBtn')!.style.border = 'solid 2px black';
+      document.getElementById('changeLanguageBtn')!.style.borderBottom = 'none';
+      document.getElementById('languageList')!.style.display = 'flex';
+      this.changeLanguageDropdownMenuIsOpen = true;
+      console.log(this.changeLanguageDropdownMenuIsOpen);
+    }
+  }
+
+
+  selectLanguage(language: string) {
+    this.selectedLanguage = language;
+    this.closeDropdown();
+  }
+
+
+  closeDropdown() {
+    if (this.changeLanguageDropdownMenuIsOpen) {
+      this.clickFunction = this.openDropdown.bind(this);
+      document.getElementById('languageList')!.style.display = 'none';
+      document.getElementById('changeLanguageBtn')!.style.border = 'solid 2px transparent';
+      this.changeLanguageDropdownMenuIsOpen = false;
+      console.log(this.changeLanguageDropdownMenuIsOpen);
+    }
+  }
+}

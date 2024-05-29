441e3dfb5f7ab920e9211b65769abfe6491a8744
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-05-29 18:30:35 +0200

    the legal notice component can now be translated

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
@@ -14,8 +16,9 @@ export class LegalNoticeComponent implements OnInit {
    * Creates an instance of LegalNoticeComponent.
    * 
    * @param {Router} router - The Angular Router service.
+   * @param {TranslateService} translate - The service used for language translation.
    */
-  constructor(private router: Router) { }
+  constructor(private router: Router, public translate: TranslateService) { }
 
 
   /**
